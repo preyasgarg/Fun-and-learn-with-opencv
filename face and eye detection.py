588c621dd3a44f7bb2556fe2b469f3e7e48116ce
preyasgarg <37271898+preyasgarg@users.noreply.github.com>
2018-09-28 01:02:51 +0530

    will detect face and eyes
    These haar files can also be made by you

---
+++ face and eye detection.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 19 15:59:20 2018
+
+@author: dpr
+"""
+
+"""
+these cascade files are already available on internet and can be freely downloaded
+"""
+
+import numpy as np
+import cv2
+
+# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+face_cascade = cv2.CascadeClassifier('F:/temp/frontalface.xml')
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
+eye_cascade = cv2.CascadeClassifier('F:/temp/eye.xml')
+watch_cascade = cv2.CascadeClassifier('F:/myhaar/data/cascade.xml')
+cap = cv2.VideoCapture(0)
+#img = cv2.imread('F:/temp/myimage3.jpg')
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    watch = watch_cascade.detectMultiScale(gray, 2)
+    
+    for (x,y,w,h) in watch:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(155,255,0),2)
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+#cap.release()
+#..cv2.waitKey(0)
+cv2.destroyAllWindows()
+#C:\Users\dpr\opencv\apps\traincascade
+#opencv_traincascade -data F:/myhaar/data -vec F:/myhaar/positives.vec -bg F:/bag.txt -numPos 1800 -numNeg 900 -numStages 10 -w 20 -h 20
