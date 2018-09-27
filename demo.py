15b7b298bf445e6db3745c7c315e2d2c19b2c60a
preyasgarg <37271898+preyasgarg@users.noreply.github.com>
2018-09-28 00:04:55 +0530

    Add files via upload
    convert input video into greyscale, makes it easy to work with

---
+++ demo.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 16 20:34:27 2018
+
+@author: dpr
+"""
+
+#press q to exit the operation or to stop the execution
+#live greyscale video through webcam
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+while(True):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    out.write(frame)
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
